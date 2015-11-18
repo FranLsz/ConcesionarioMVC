fa6f2a49b5ba5c784710c390616b4e18ad5cf3e4
FranLsz <franlsz95develop@gmail.com>
2015-11-18 11:16:15 +0100

    first commit

---
+++ Concesionario/Controllers/TipoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Concesionario.Models;
+
+namespace Concesionario.Controllers
+{
+    public class TipoController : Controller
+    {
+        Concesionario15Entities db = new Concesionario15Entities();
+
+        // GET: Tipo
+        public ActionResult Index()
+        {
+            return View(db.Tipo);
+        }
+
+        
+    }
+}
---
+++ Concesionario/Controllers/VehiculoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Concesionario.Models;
+
+namespace Concesionario.Controllers
+{
+    public class VehiculoController : Controller
+    {
+        Concesionario15Entities db = new Concesionario15Entities();
+
+        // GET: Vehiculo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ListadoPorTipo(int id)
+        {
+
+            return View(db.Vehiculo.Where(o=>o.tipo == id));
+        }
+    }
+}
